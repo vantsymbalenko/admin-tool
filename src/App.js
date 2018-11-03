b2aff54f3f66e089332ab17086554121c22f7848
Ivan Tsymbalenko <IvanZymbalenko1@gmail.com>
2018-11-04 00:04:51 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      show : false
+    }
+  }
+
+  isShow = () => {
+    this.setState({
+        show : true
+    });
+  };
+
+  isHide = () => {
+    this.setState({
+        show: false
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className={"hover"} onMouseOver={this.isShow} onMouseOut={this.isHide}>
+        adasdad
+        <div className={this.state.show ? "show" : "hide"}>
+          this is tooltip
+        </div>
       </div>
     );
   }
